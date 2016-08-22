58bb14fad53912c9b27d67ce9bda2ffbf3c6f780
mariejinpark <mariejinpark@gmail.com>
2016-08-22 14:19:48 -0400

    Complete problems

--- Arrays.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ Arrays.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,7 +13,7 @@
  */
 // write your code here
 
-
+var list = ["Bread", "Butter", "Cheese", "Lettuce", "Tomatoes"]
 
 
 
@@ -23,7 +23,7 @@
  */
 // write your code here
 
-
+var shoppingList: [String] = ["Bread", "Butter", "Cheese", "Lettuce", "Tomatoes"]
 
 
 
@@ -33,7 +33,7 @@
 // write your code here
 
 
-
+var futureShoppingList: [String] = ["Bread", "Butter", "Cheese", "Lettuce", "Tomatoes"]
 
 
 
@@ -42,7 +42,7 @@
  */
 // write your code here
 
-
+let cheeseSandwich: [String] = ["Bread", "Butter", "Cheese", "Lettuce", "Tomatoes"]
 
 
 
@@ -53,8 +53,7 @@
 // write your code here
 
 
-
-
+var dessertList: [String] = ["Cookie dough", "Icecream"]
 
 
 
@@ -64,7 +63,7 @@
 // write your code here
 
 
-
+var afternoonAttendance: [Int] = [2, 10, 3, 15, 7]
 
 
 
@@ -76,7 +75,8 @@
 // write your code here
 
 
-
+var itemToPrint = shoppingList[1]
+print(itemToPrint)
 
 
 
@@ -88,8 +88,8 @@
 // write your code here
 
 
-
-
+futureShoppingList[2] = "Chicken"
+print(futureShoppingList)
 
 
 
@@ -100,7 +100,13 @@
 // write your code here
 
 
-
+func lifesEssential (ingredients:[String]) -> Bool {
+    if ingredients[0] == "Bread"{
+        return true
+    } else {
+        return false
+    }
+}
 
 
 
@@ -109,7 +115,7 @@
  */
 // write your code here
 
-
+print(lifesEssential(shoppingList))
 
 
 
@@ -121,7 +127,7 @@
  */
 // write your code here
 
-
+print(lifesEssential(dessertList))
 
 
 
