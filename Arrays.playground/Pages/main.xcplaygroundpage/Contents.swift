c10407b0eec963406a2add122d25c443297a987b
Barry Levy <barry.levy@gmail.com>
2017-01-07 16:17:18 -0500

    Complete problems

--- Arrays.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ Arrays.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -14,7 +14,7 @@
 // write your code here
 
 
-
+var list = ["Bread", "Butter", "Cheese", "Lettuce", "Tomatoes"]
 
 
 
@@ -23,7 +23,7 @@
  */
 // write your code here
 
-
+var shoppingList: [String] = ["Bread", "Butter", "Cheese", "Lettuce", "Tomatoes"]
 
 
 
@@ -33,7 +33,9 @@
 // write your code here
 
 
+var futureShoppingList: [String] = []
 
+futureShoppingList = ["Bread", "Butter", "Cheese", "Lettuce", "Tomatoes"]
 
 
 
@@ -43,7 +45,7 @@
 // write your code here
 
 
-
+let cheeseSandwich: [String] = ["Bread", "Butter", "Cheese", "Lettuce", "Tomatoes"]
 
 
 
@@ -53,8 +55,7 @@
 // write your code here
 
 
-
-
+let dessertList: [String] = ["Cookie dough", "Ice Cream"]
 
 
 
@@ -64,9 +65,7 @@
 // write your code here
 
 
-
-
-
+var afternoonAttendance: [Int] = [2, 10, 3, 15, 7]
 
 
 
@@ -77,9 +76,9 @@
 
 
 
+var itemToPrint = shoppingList[2]
 
-
-
+print(itemToPrint)
 
 
 /*: question8
@@ -88,9 +87,9 @@
 // write your code here
 
 
+shoppingList[2] = "Chicken"
 
-
-
+print(shoppingList)
 
 
 
@@ -98,6 +97,12 @@
  ### 9. Create a function named 'lifesEssential' that accepts a string of arrays as a parameter called 'ingredients' and returns a boolean. Inside the function check to see if the array passed to the function contains the value "Bread" at position 0. If it does, return a true value boolean, if not false.
  */
 // write your code here
+
+func lifesEssential(ingredients:Array<String>) -> Bool  {
+    if ingredients[0] == "Bread" {
+        return(true)
+    } else {return(false)}
+}
 
 
 
@@ -110,7 +115,7 @@
 // write your code here
 
 
-
+print(lifesEssential(ingredients: shoppingList))
 
 
 
@@ -121,6 +126,8 @@
  */
 // write your code here
 
+
+print(lifesEssential(ingredients: dessertList))
 
 
 
